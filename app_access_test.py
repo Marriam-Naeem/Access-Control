bad20bf61050d98ba997e33f1253f78af7d4a262
Sadia Rehman <77920869+sadiarehman@users.noreply.github.com>
2023-12-25 19:09:40 +0500

    Add files via upload

---
+++ app_access_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from app_test import FlaskAppTests
+
+from app import grant_access
+
+
+class AccessTests(unittest.TestCase):
+    def test_grant_access_function(self):
+        # Test grant_access function with valid inputs
+        self.assertTrue(grant_access('Task', 'sanwaar.bese20seecs@seecs.edu.pk'))
+
+        # Test grant_access function with invalid inputs
+        self.assertFalse(grant_access('InvalidResource', 'zanwaar.bese20seecs@seecs.edu.pk'))
+
+#since grant_access checks working hours too, returns True if resource and email matches AND working hours, location are valid
+if __name__ == '__main__':
+    unittest.main()
---
+++ app_test.py
@@ -0,0 +1,143 @@
+import socket
+import unittest
+from datetime import datetime
+from unittest.mock import patch
+
+from flask import Flask
+
+from app import (LOGIN_ATTEMPTS, app, authenticate_user, check_location_access,
+                 check_working_hours, generate_otp, get_attributes_dict,
+                 get_stored_otp, grant_access, increment_failed_login_attempts,
+                 send_otp_email, set_stored_otp)
+
+
+class FlaskAppTests(unittest.TestCase):
+    
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def test_main_page(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_authentication(self):
+        response = self.app.post('/', data=dict(Email='invalid@example.com', Password='wrongpassword'))
+        self.assertIn(b'Authentication failed', response.data)
+
+    def test_valid_authentication(self):
+        response = self.app.post('/', data=dict(Email='zanwaar.bese20seecs@seecs.edu.pk', Password='password'))
+    
+    
+        self.assertEqual(response.status_code, 200)
+        
+        self.assertIn(b'<button type="submit">Verify OTP and Grant Access</button>', response.data)
+        
+
+    def test_otp_verification(self):
+        # Assuming a valid OTP is generated
+        valid_otp = generate_otp()
+
+        # Set the stored OTP for verification
+        set_stored_otp(valid_otp)
+
+        # Verify OTP endpoint
+        response = self.app.post('/verify_otp_and_grant_access', data=dict(otp=valid_otp, resource='Project'))
+        self.assertIn(b'Access denied', response.data)
+
+    def test_access_denied(self):
+        # Assuming a valid OTP is generated
+        valid_otp = generate_otp()
+
+        # Set the stored OTP for verification
+        set_stored_otp(valid_otp)
+
+        # Verify OTP endpoint with incorrect resource
+        response = self.app.post('/verify_otp_and_grant_access', data=dict(otp=valid_otp, resource='InvalidResource'))
+        self.assertIn(b'Access denied', response.data)
+
+
+    def test_check_location_access(self):
+        # Assuming the host IP is the required IP
+        with self.subTest(msg="Valid IP Address"):
+            self.assertTrue(check_location_access())
+
+        # Assuming the host IP is not the required IP
+        with self.subTest(msg="Invalid IP Address"):
+            socket.gethostbyname = lambda x: '192.167.1.1'  # an invalid IP
+            self.assertFalse(check_location_access())
+
+    
+    @patch('app.datetime', autospec=True)
+    def test_check_working_hours(self,mocked_datetime):
+        # Assuming the current time is within working hours
+            mocked_datetime.now.return_value = datetime(2023, 1, 1, 9, 0, 0)  # January 1, 2023, 9:00 AM
+            print(check_working_hours())
+            self.assertTrue(check_working_hours())
+
+        # Assuming the current time is outside working hours
+        
+            mocked_datetime.now.return_value = datetime(2023, 1, 1, 18, 0, 0)  # January 1, 2023, 6:00 PM
+            print(check_working_hours())
+            self.assertFalse(check_working_hours())
+
+
+
+#-------------------------------------------------------------
+    def test_successful_login_and_otp_verification(self):
+        with patch('app.authenticate_user', return_value=True):
+            response_login = self.app.post('/', data=dict(Email='zanwaar.bese20seecs@seecs.edu.pk', Password='password'))
+            self.assertEqual(response_login.status_code, 200)
+            self.assertIn(b'OTP Verification', response_login.data)
+
+        with patch('app.generate_otp', return_value='123456'), \
+            patch('app.send_otp_email'), \
+            patch('app.get_stored_otp', return_value='123456'), \
+            patch('app.grant_access', return_value=True):
+
+            response_verify_otp = self.app.post('/verify_otp_and_grant_access',
+                                                data=dict(otp='123456', resource='Project'))
+            self.assertEqual(response_verify_otp.status_code, 200)
+            self.assertIn(b'Access granted', response_verify_otp.data)
+
+    def test_unsuccessful_login_incorrect_credentials(self):
+        with patch('app.authenticate_user', return_value=False):
+            response = self.app.post('/', data=dict(Email='zanwaar.bese20seecs@seecs.edu.pk', Password='wrong_password'))
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'Authentication failed. Please check your credentials.', response.data)
+
+    def test_unsuccessful_login_account_locked(self):
+        # Set the number of login attempts to reach the lockout threshold
+        LOGIN_ATTEMPTS['zanwaar.bese20seecs@seecs.edu.pk'] = {'attempts': 2, 'timestamp': '2023-01-01 00:00:00.000'}
+
+        with patch('app.is_user_locked_out', return_value=True):
+            response = self.app.post('/', data=dict(Email='zanwaar.bese20seecs@seecs.edu.pk', Password='wrong_password'))
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'Account locked due to multiple unsuccessful login attempts. Please try again later.', response.data)
+
+    def test_unsuccessful_otp_verification(self):
+        with patch('app.get_stored_otp', return_value='654321'), \
+            patch('app.grant_access', return_value=False):
+
+            response = self.app.post('/verify_otp_and_grant_access',data=dict(otp='123456', resource='Project'))
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'Authentication failed due to Incorrect OTP', response.data)
+
+
+    def test_get_attributes_dict(self):
+            # Arrange
+            expected_users_info = [
+                {'email': 'zanwaar.bese20seecs@seecs.edu.pk', 'password': 'password', 'username': 'zanwaarpassword', 'MFA_Enabled': True, 'IPv4': '39.58.249.103', 'Role': 'Project_Manager', 'OTP': ''},
+                {'email': 'jbfjdhanwaar.bese20seecs@seecs.edu.pk', 'password': 'password', 'username': 'zanwaarpassword', 'MFA_Enabled': True, 'IPv4': '39.58.249.103', 'Role': 'Project_Manager', 'OTP': ''},
+                {'email': 'skdjskanwaar.bese20seecs@seecs.edu.pk', 'password': 'password', 'username': 'zanwaarpassword', 'MFA_Enabled': True, 'IPv4': '39.58.249.103', 'Role': 'Project_Manager', 'OTP': ''},
+                {'email': 'tanwaar.bese20seecs@seecs.edu.pk', 'password': 'password', 'username': 'zanwaarpassword', 'MFA_Enabled': True, 'IPv4': '39.58.249.103', 'Role': 'Developer', 'OTP': ''},
+                {'email': 'sanwaar.bese20seecs@seecs.edu.pk', 'password': 'password', 'username': 'sanwaarpassword', 'MFA_Enabled': True, 'IPv4': '39.58.249.103', 'Role': 'Developer', 'OTP': ''}
+            ]
+
+            # Act
+            result = get_attributes_dict()
+
+            # Assert
+            self.assertEqual(result, expected_users_info)
+if __name__ == '__main__':
+    unittest.main()
