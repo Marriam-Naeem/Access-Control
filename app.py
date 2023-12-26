5056d40aca081af56b6273332232d28b3ef7340c
Zainab Anwaar <pisces.zaiby03@gmail.com>
2023-12-26 09:28:05 +0500

    Format Code

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-#brute force
-#traffic analysis
-
+#Libraries
 from datetime import datetime, time, timedelta
 import flask
 import random
@@ -10,47 +8,46 @@ import hashlib
 from flask_mail import Mail, Message
 
 app = flask.Flask(__name__, template_folder='templates', static_folder='static')
-LOGIN_ATTEMPTS = {}  # Dictionary to store failed login attempts with timestamps
+# Dictionary to store failed login attempts with timestamps
+LOGIN_ATTEMPTS = {} 
 
 # Flask-Mail configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'pisces.zaiby03@gmail.com'  # Replace with your Gmail email
-app.config['MAIL_PASSWORD'] = 'yhqq uoji swbj qfbq'  # Replace with your Gmail password
+app.config['MAIL_USERNAME'] = 'pisces.zaiby03@gmail.com'  
+app.config['MAIL_PASSWORD'] = 'yhqq uoji swbj qfbq'
 
 mail = Mail(app)
 STORED_OTP = ''
 EMAIL=''
-LOCKOUT_THRESHOLD = 3  # Number of allowed unsuccessful login attempts
-LOCKOUT_TIMEFRAME = timedelta(minutes=5)  # Time frame for counting failed attempts
-LOCKOUT_DURATION = timedelta(minutes=10)  # Lockout duration if the threshold is reached
+# Constants for lockout mechanism
+LOCKOUT_THRESHOLD = 3  
+LOCKOUT_TIMEFRAME = timedelta(minutes=5)
+LOCKOUT_DURATION = timedelta(minutes=10)  
 
-
+#Utility Functions for locking feature
 def increment_failed_login_attempts(email):
     if email in LOGIN_ATTEMPTS:
         LOGIN_ATTEMPTS[email]['attempts'] += 1
     else:
         LOGIN_ATTEMPTS[email] = {'attempts': 1, 'timestamp': datetime.now()}
 
-
 def reset_failed_login_attempts(email):
     if email in LOGIN_ATTEMPTS:
         del LOGIN_ATTEMPTS[email]
-
 
 def is_user_locked_out(email):
     if email in LOGIN_ATTEMPTS:
         attempts_info = LOGIN_ATTEMPTS[email]
         timestamp = attempts_info['timestamp']
         attempts = attempts_info['attempts']
-
         # Check if the threshold is reached within the specified timeframe
         if attempts >= LOCKOUT_THRESHOLD and datetime.now() - timestamp < LOCKOUT_TIMEFRAME:
             return True
-
     return False
 
+# Utility Functions for Access Control: Location
 def check_location_access():
     def get_first_two_octets(ip_address):
         return '.'.join(ip_address.split('.')[:2])
@@ -59,19 +56,22 @@ def check_location_access():
     required_ip = "192.168.100.12"
     return get_first_two_octets(host_ip_address) == get_first_two_octets(required_ip)
 
+# Utility Functions for Access Control: Time
 def check_working_hours():
     now = datetime.now().time()
     start_time = time(9, 0, 0)
     end_time = time(18, 0, 0)
     return start_time <= now <= end_time
 
+# Utility Functions for Access Control: Role
 def get_user_role(email):
     users_info = get_attributes_dict()
     for user_info in users_info:
         if user_info['email'] == email:
             return user_info['Role']
-    return None  # Return None if the email is not found
+    return None  
 
+# Utility Functions for Access Control: Resource
 def grant_access(resource, email):
     if (check_location_access()) and (check_working_hours()):
         role = get_user_role(email)
@@ -98,6 +98,7 @@ def grant_access(resource, email):
     else:
         return False
 
+# Utility Functions for OTP
 def generate_otp():
     return ''.join(random.choices(string.digits, k=6))
 
@@ -113,6 +114,7 @@ def set_stored_otp(otp):
 def get_stored_otp():
     return STORED_OTP
 
+# Flask App Routes
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if flask.request.method == 'GET':
@@ -122,14 +124,11 @@ def main():
         global EMAIL
         EMAIL = email
         password = flask.request.form['Password']
-
         # Check if the user is locked out
         if is_user_locked_out(email):
             return flask.render_template('index.html', message='Account locked due to multiple unsuccessful login attempts. Please try again later.')
-
         # Authenticate the user using AccessControl
         auth_result = authenticate_user(email, password)
-
         if auth_result:
             reset_failed_login_attempts(email)  # Reset failed login attempts upon successful login
             otp = generate_otp()
@@ -160,7 +159,7 @@ def verify_otp_and_grant_access():
     else:
         return flask.render_template('index.html', message='Authentication failed due to Incorrect OTP')
 
-
+# Utility Function for Authentication
 def authenticate_user(email, password):
     users_info = get_attributes_dict()
     hashed_password = hashlib.sha3_256(password.encode()).hexdigest()
@@ -169,6 +168,7 @@ def authenticate_user(email, password):
             return True
     return False
 
+# Utility Function for getting user attributes
 def get_attributes_dict():
     users_info = [
         {
